1de0cb2277c5ce830288bf3b0bda3a8d9c01277c
tshrrx <144604940+tshrrx@users.noreply.github.com>
2025-06-28 13:57:10 +0530

    SpringBoot Setup

---
+++ src/main/java/com/shrinkerkit/shrinker_kit_backend/ShrinkerKitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shrinkerkit.shrinker_kit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrinkerKitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrinkerKitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shrinkerkit/shrinker_kit_backend/ShrinkerKitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrinkerkit.shrinker_kit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShrinkerKitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
